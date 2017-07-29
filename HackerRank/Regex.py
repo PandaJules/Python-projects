50da24bef20f52ea5f88d49fbcfe7b2d910d001e
PandaJules <yb259@cam.ac.uk>
2017-07-29 21:20:10 +0200

    more regex

--- HackerRank/Regex.py
+++ HackerRank/Regex.py
@@ -3,6 +3,8 @@ import re
 # \s matches any whitespace character \r\n\t\f
 # \d matched any digit 0-9
 # \w will match any word character: alphanumeric characters and underscores
+# [^] matches whatever is NOT inside of brackets
+# X{n} matches X n times
 
 test_string1 = input()
 regex_pattern = r'the'
@@ -27,3 +29,5 @@ regex_pattern = r'\S\S\S\s\S\S\S$'
 match = re.search(regex_pattern, test_string4).group()
 print("Number of matches:   ", len(match))
 
+regex_pattern1 = r'\.{2,5}$'	# from 2 to 5 dots inclusively
+regex_pattern2 = r'\d{3,}$'		# 3 or more digits
