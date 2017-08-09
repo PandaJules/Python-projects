919f82707d3b9365bc16c2a3507cc1790d708812
PandaJules <yb259@cam.ac.uk>
2017-08-09 15:38:08 +0200

    String formatting

--- HackerRank/OneLiners.py
+++ HackerRank/OneLiners.py
@@ -10,3 +10,33 @@ def triples():
 def second_max():
     nums = map(int, input().split())
     print(sorted(list(set(nums)))[-2])
+
+
+def swapcase(s):
+    return s.swapcase()
+
+
+def swapcase2():
+    return ''.join([i.lower() if i.isupper() else i.upper() for i in input()])
+
+
+def split_and_join(s):
+    return "-".join(s.split(" "))
+
+
+def welcome():
+    firstname, lastname = input("What is your full name?  ").split()
+    print("Hello {0} {1}! This line is done with new formatting!".format(firstname, lastname))
+    print("But did you know, %s, that depricated formatting still works!" % firstname)
+
+
+def count_substring(string, substring):
+    # len(string) - len(substring) + 1 == count for the sliding window
+    # string[i:i + len(substring)] == slice of the string that is currently compared to the substring
+    return sum([1 for i in range(len(string) - len(substring) + 1) if string[i:i + len(substring)] == substring])
+
+
+def string_checker(string):
+    for test in ('isalnum', 'isalpha', 'isdigit', 'islower', 'isupper'):
+        print("For {0} test and {1} string:".format(test, string), any(eval("char." + test + "()") for char in string))
+
