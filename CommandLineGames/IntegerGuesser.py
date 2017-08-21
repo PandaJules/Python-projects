e82333fb2f249d666571af049b4150f06a4a5b97
PandaJules <yb259@cam.ac.uk>
2017-08-21 18:39:22 +0200

    Change of range and stylistic changes

--- CommandLineGames/IntegerGuesser.py
+++ CommandLineGames/IntegerGuesser.py
@@ -2,12 +2,12 @@ import sys
 from random import randint
 play = True
 while play:
-    secret = randint(0, 4)
-    guess = input("I thought of a number. Guess it:  ")
+    secret = randint(1, 5)
+    guess = input("I thought of a number from 1 to 5. Guess it:  ")
     if str(secret) == guess:
-        print ("Wow, you guessed it!")
+        print("Wow, you guessed it!")
     else:
-        print ("Too bad :(. It was {}".format(secret))
+        print("Too bad :(. It was {}".format(secret))
         answer = input("Would you like to play again?(Enter 'y' for yes; 'n' for no): ")
         if answer == 'n':
             print("Thanks for playing.")
