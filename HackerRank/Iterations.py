68547a70ce7417e8948991c94b557d56de22997a
PandaJules <yb259@cam.ac.uk>
2017-08-26 23:35:44 +0200

    Lambda, reduce

--- HackerRank/Iterations.py
+++ HackerRank/Iterations.py
@@ -1,6 +1,7 @@
 from itertools import product
 from itertools import permutations
 from itertools import combinations
+from functools import reduce
 
 
 def cross_product(list1, list2):
@@ -24,3 +25,19 @@ def frequencies():
     # output (frequency, symbol) for consecutive repeated symbols
     from itertools import groupby
     print(*[(len(list(c)), int(k)) for k, c in groupby(input())])
+
+
+def n_fibonacci_to_m(n, m):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i - 2] + fib[i - 1])
+    cube = lambda x: pow(x, m)
+    print("List of", n, "fibonacci numbers to the power", m, "is\n", list(map(cube, fib)))
+
+
+def list_sum(lst):
+    print("Sum of the list", lst, "is\n", reduce(lambda x, y: x + y, lst))
+
+
+def list_product(lst):
+    print("Product of the list", lst, "is\n", reduce(lambda x, y: x * y, lst))
